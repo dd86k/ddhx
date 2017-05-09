ec9182d427f904923befd9d52dce03c3e815354b
dd <devddstuff@gmail.com>
2017-05-09 16:52:00 -0400

    Piece of fucking SHIT

--- src/Searcher.d
+++ src/Searcher.d
@@ -8,19 +8,20 @@ private enum CHUNK_SIZE = 2 * MB;
 
 void SearchByte(const ubyte b)
 {
+    //TODO: Fix byte searching
     MessageAlt("Searching byte...");
     long pos = CurrentPosition;
-    foreach (const ubyte[] buf; CurrentFile.byChunk(CHUNK_SIZE))
-    {
-        for (int i; i < buf.length; ++i)
+    foreach (buf; CurrentFile.byChunk(CHUNK_SIZE)) {
+        for (int i; i < buf.length; ++i) {
             if (b == buf[i]) {
                 import std.format : format;
-                long l = pos + i + Buffer.length;
+                long l = pos + Buffer.length;
                 Goto(l);
                 MessageAlt(format(" Found byte %02XH at %XH", b, l));
                 return;
             }
-        pos += CHUNK_SIZE;
+            ++pos;
+        }
     }
     MessageAlt("Not found");
 }
@@ -37,9 +38,8 @@ void SearchUTF8String(const char[] s)
     long pos = CurrentPosition;
     //TODO: Fix string searching
     //TODO: String compare if between chunks
-    foreach (const ubyte[] buf; CurrentFile.byChunk(CHUNK_SIZE))
-    {
-        for (int i; i < buf.length; ++i, ++pos) {
+    foreach (buf; CurrentFile.byChunk(CHUNK_SIZE)) {
+        for (int i; i < buf.length; ++i) {
             if (b == buf[i]) { // OK
                 if (buf[i..i+len+1] == s) { // Doesn't work
                     import std.format : format;
@@ -49,6 +49,7 @@ void SearchUTF8String(const char[] s)
                     return;
                 }
             }
+            ++pos;
         }
     }
     MessageAlt("Not found");
