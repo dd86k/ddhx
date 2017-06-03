aaf037f5b4da0f77b77c4e7788b883815575251c
dd <devddstuff@gmail.com>
2017-06-03 19:45:42 -0400

    -w a on Windows

--- src/SettingHandler.d
+++ src/SettingHandler.d
@@ -30,7 +30,13 @@ void HandleWidth(string val, bool cli = false)
 {
 	import ddcon : WindowWidth;
 	if (val[0] == 'a') {
+		version (Windows) {
+//TODO: Fix with CLI, returns 65535 (why can't Windows work?)
+		if (!cli)
+			BytesPerRow = cast(ushort)(((WindowWidth - 10) / 4) - 1);
+		} else {
 		BytesPerRow = cast(ushort)(((WindowWidth - 10) / 4) - 1);
+		}
 	} else {
 		long l;
 		if (unformat(val, l)) {
@@ -40,6 +46,7 @@ void HandleWidth(string val, bool cli = false)
 					exit(1);
 				} else
 					MessageAlt("Number out of range");
+					return;
 			}
 			BytesPerRow = l & 0xFFFF;
 		} else {
