bdcc841bdce208fd0b06d965cca7ae96e59b836c
dd86k <dd@dax.moe>
2025-08-30 15:48:54 -0400

    Bump to 0.5.0-alpha.1

--- src/ddhx.d
+++ src/ddhx.d
@@ -21,10 +21,12 @@ import transcoder;
 // TODO: Find a way to dump session data to be able to resume later
 //       Session/project whatever
 
+private debug enum DEBUG = "+debug"; else enum DEBUG = "";
+
 /// Copyright string
 immutable string DDHX_COPYRIGHT = "Copyright (c) 2017-2025 dd86k <dd@dax.moe>";
 /// App version
-immutable string DDHX_VERSION   = "0.5.0";
+immutable string DDHX_VERSION   = "0.5.0-alpha.1"~DEBUG;
 /// Build information
 immutable string DDHX_BUILDINFO = "Built: "~__TIMESTAMP__;
 
