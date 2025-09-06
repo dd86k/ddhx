a7ec4f7ef4d33fc8ee05a2793c23c306353cafd1
dd86k <dd@dax.moe>
2025-09-06 08:21:15 -0400

    utils: scan function to parse dec/hex/oct integers

--- src/utils.d
+++ src/utils.d
@@ -83,3 +83,39 @@ string[] arguments(const(char)[] buffer)
     assert(arguments(`A           B`) == [ "A", "B" ]);
     //assert(arguments(`tab\tmoment`) == [ "tab", "moment" ]);
 }
+
+long scan(scope string input)
+{
+    // std.format.read, std.conv.to, and std.conv.parse makes this harder
+    // than it should be...
+    
+    import core.stdc.stdlib : strtoll;
+    import core.stdc.errno : errno;
+    import core.stdc.string : strerror;
+    import std.string : toStringz, fromStringz;
+    
+    errno = 0;
+    long i = strtoll(toStringz(input), null, 0);
+    if (errno)
+        throw new Exception(cast(string)fromStringz(strerror(errno)));
+    
+    return i;
+}
+@system unittest
+{
+    import std.conv : octal;
+    
+    assert(scan("0") == 0);
+    // decimal
+    assert(scan("1") == 1);
+    assert(scan("2") == 2);
+    assert(scan("10") == 10);
+    // hex
+    assert(scan("0x1")  == 0x1);
+    assert(scan("0x2")  == 0x2);
+    assert(scan("0x10") == 0x10);
+    // octal
+    assert(scan("01")  == octal!"1");
+    assert(scan("02")  == octal!"2");
+    assert(scan("010") == octal!"10");
+}
