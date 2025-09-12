1b6bbd7e1e38da59bc84d4f3d39c3a1f748c3100
dd86k <dd@dax.moe>
2025-09-12 00:22:13 -0400

    Bump version to 0.5.0-beta.1

--- src/ddhx.d
+++ src/ddhx.d
@@ -26,7 +26,7 @@ private debug enum DEBUG = "+debug"; else enum DEBUG = "";
 /// Copyright string
 immutable string DDHX_COPYRIGHT = "Copyright (c) 2017-2025 dd86k <dd@dax.moe>";
 /// App version
-immutable string DDHX_VERSION   = "0.5.0-alpha.3"~DEBUG;
+immutable string DDHX_VERSION   = "0.5.0-beta.1"~DEBUG;
 /// Build information
 immutable string DDHX_BUILDINFO = "Built: "~__TIMESTAMP__;
 
