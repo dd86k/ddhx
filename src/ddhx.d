63b4133bf4dd1f9aa1a834960d3827889bcffc80
dd86k <dd@dax.moe>
2025-11-08 08:45:41 -0500

    Bump 0.7.1

--- src/ddhx.d
+++ src/ddhx.d
@@ -25,7 +25,7 @@ private debug enum DEBUG = "+debug"; else enum DEBUG = "";
 /// Copyright string
 immutable string DDHX_COPYRIGHT = "Copyright (c) 2017-2025 dd86k <dd@dax.moe>";
 /// App version
-immutable string DDHX_VERSION   = "0.7.0"~DEBUG;
+immutable string DDHX_VERSION   = "0.7.1"~DEBUG;
 /// Build information
 immutable string DDHX_BUILDINFO = "Built: "~__TIMESTAMP__;
 
