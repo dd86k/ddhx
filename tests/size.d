23d94d11e62d47e7773555bc180f947ada91c8fc
dd86k <dd@dax.moe>
2025-11-23 22:34:01 -0500

    dub: "size" buildType to test term size/cursor pos, remove piece test

---
+++ tests/size.d
@@ -0,0 +1,15 @@
+module tests.size;
+
+import std.stdio;
+import os.terminal;
+
+@system unittest {
+    terminalInit(TermFeat.rawInput); // hack for terminalTell
+    scope(exit) terminalRestore();
+    
+    TerminalSize size = terminalSize();
+    writeln("Size: COLS=", size.columns, " ROWS=", size.rows);
+    
+    TerminalPosition pos = terminalTell();
+    writeln("Position: COL=", pos.column, " ROW=", pos.row);
+}
