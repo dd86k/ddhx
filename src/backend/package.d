5d53e29cb45ad30a77242001d57367327a136d22
dd86k <dd@dax.moe>
2026-01-12 11:31:05 -0500

    Remove "chunk" backend

--- src/backend/package.d
+++ src/backend/package.d
@@ -5,4 +5,6 @@
 /// Authors: $(LINK2 https://github.com/dd86k, dd86k)
 module backend;
 
-public import backend.base, backend.chunk, backend.piece, backend.piecev2;
+public import backend.base : IDocumentEditor, selectBackend;
+public import backend.piece : PieceDocumentEditor;
+public import backend.piecev2 : PieceV2DocumentEditor;
