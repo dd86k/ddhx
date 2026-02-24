ef4d0ee360fd53a331f5251fe23ed6016e7a7b8d
dd86k <dd@dax.moe>
2026-02-24 17:01:11 -0500

    Bump 0.9.0

--- src/ddhx.d
+++ src/ddhx.d
@@ -32,7 +32,7 @@ private debug enum DEBUG = "+debug"; else enum DEBUG = "";
 /// Copyright string
 immutable string DDHX_COPYRIGHT = "Copyright (c) 2017-2026 dd86k <dd@dax.moe>";
 /// App version
-immutable string DDHX_VERSION   = "0.8.3"~DEBUG;
+immutable string DDHX_VERSION   = "0.9.0"~DEBUG;
 /// Build information
 immutable string DDHX_BUILDINFO = "Built: "~__TIMESTAMP__;
 
